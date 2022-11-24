a4a37baa0ea308ce35cce6e36725c9051cf96acc
songhengqiang <15292242950@163.com>
2022-11-24 14:53:03 +0800

    first commit

---
+++ nacosTest/src/main/java/com/shq/nacosTest/NacosTestApplication.java
@@ -0,0 +1,12 @@
+package com.shq.nacosTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosTestApplication.class);
+    }
+}
